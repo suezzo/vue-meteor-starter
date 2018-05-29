3f07aeaa03cce79c7346107c98b89dc39fe5fbfb
Michał Parkitny <misiek4888@gmail.com>
2018-05-29 13:50:54 +0200

    Router group routes from multiple modules and then merge then in one final object containing routes.

--- imports/modules/router/index.js
+++ imports/modules/router/index.js
@@ -1,4 +1,5 @@
 import coreRoutes from '/imports/modules/core/client/routes.js'
+// import ordersRoutes from '/imports/modules/orders/client/routes.js'
 
-const Routes = coreRoutes
+// const Routes = coreRoutes.concat(ordersRoutes)
 export default Routes
