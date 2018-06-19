7aa57606be31b2ce291554194f0b5610ee8185f9
Michał Parkitny <misiek4888@gmail.com>
2018-06-19 13:40:39 +0200

    Updated meteor to 1.7.0.3. Removed underscore package from Meteor. If needed package from npm should be used.
    Quick routes fix has been added (not important).

--- imports/modules/router/index.js
+++ imports/modules/router/index.js
@@ -2,4 +2,6 @@ import coreRoutes from '/imports/modules/core/client/routes.js'
 // import ordersRoutes from '/imports/modules/orders/client/routes.js'
 
 // const Routes = coreRoutes.concat(ordersRoutes)
+const Routes = coreRoutes
+
 export default Routes
